0624d7a30dd89ebc58d2de248752692864a453a4
Tommy Savela <tommy.savela@stenaline.com>
2021-10-25 11:49:17 +0200

    Initial commit

---
+++ Api/Controllers/EchoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Scheme: {Request.Scheme}");
+            sb.AppendLine($"Host: {Request.Host}");
+            sb.AppendLine("Headers:");
+            foreach (var header in Request.Headers)
+            {
+                sb.AppendLine($"{header.Key}: {header.Value}");
+            };
+
+            return sb.ToString();
+        }
+    }
+}
